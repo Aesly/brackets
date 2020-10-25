97fa12db0d5eee5a92642788df8ce32e18bbaec0
Kurchanov Vladislav <dikdamned@gmail.com>
2020-10-25 23:23:41 +0300

    init: do  brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let bracketsConfigMod = bracketsConfig.map(function(array){
+      let stringFromArray = array[0] + array[1];
+      return stringFromArray;
+  });
+  
+  while (bracketsConfigMod.some(elem => str.indexOf(elem) >= 0)) {
+      for (i = 0; i <= bracketsConfigMod.length - 1; i += 1) {
+          while (str.indexOf(bracketsConfigMod[i]) >= 0) {
+              str = str.replace(bracketsConfigMod[i], '');
+          }
+      }
+  }
+  
+      if (str.length === 0) {
+          return true;
+      } else {
+          return false;
+      }
 }
